cecfecdeecb84f62a0e5b8a98ca90be3f7123fed
robertosilvatim <roberto.silva.ti@gmail.com>
2023-11-30 16:27:18 -0300

    update files statics, img and tamplate

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
-# Todas as rotas relcionadas a geleria.
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('imagem/', imagem),
 ]
