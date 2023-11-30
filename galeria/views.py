18c2a38fa748693a1c704292808d6cc4e1d559a3
robertosilvatim <roberto.silva.ti@gmail.com>
2023-11-30 13:06:22 -0300

    add folder templates

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Ola deu certo</h1>')
+    return render(request, 'index.html')
